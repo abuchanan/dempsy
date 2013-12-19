04c36d78e5de3d32bb0c8db86a23ef26deffa37d
Alex Buchanan <buchanae@gmail.com>
2013-12-18 23:41:03 -0800

    ignore modified (ctrl, alt, shift, etc) input

--- app/scripts/editor.js
+++ app/scripts/editor.js
@@ -2,12 +2,22 @@
 
 var mod = angular.module('dempsy.editor', []);
 
+
 mod.service('Editor', function($document, $rootScope) {
 
   var scope = $rootScope.$new(true);
 
+  function modified() {
+    console.log(altModified, ctrlModified, metaModified, shiftModified);
+    return altModified || ctrlModified || metaModified || shiftModified;
+  }
+
   // TODO this should be bound/unbound on selectedManager active/inactive events
   $document.keypress(function(event) {
+    // If the user is holding a modifier key (ctrl, shift, etc)
+    // don't accept character input.
+    if (event.altKey || event.ctrlKey || event.metaKey || event.shiftKey) return;
+
     if (event.which != 0 && event.charCode != 0) {
       var c = String.fromCharCode(event.which);
       scope.$apply(function() {
